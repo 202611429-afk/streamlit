9353f0a8852792aff11542685fa97fc47898d272
202611429-afk <2026_11429@kshcm.net>
2026-05-20 12:02:16 +0700

    Add files via upload

--- weight_app.py
+++ weight_app.py
@@ -7,13 +7,13 @@ import joblib
 st.title("신체 정보를 이용한 몸무게 예측 머신러닝 모델")
 st.write("신체 정보를 입력하면 몸무게를 예측합니다.")
 
-model = joblib.load("weight_model_male.pkl")
+model = joblib.load("weight_model_male (1).pkl")
 
 st.sidebar.header("머신러닝 모델 설계 실습 (다중회귀)")
 
 height = st.slider("키 (cm)", 140.0, 190.0, 170.0)
 waist = st.slider("허리 둘레 (cm)", 50.0, 120.0, 80.0)
-hip = st.slider("엉덩이 둘레 (cm)", 85.0, 120.0, 100.0)
+hip = st.slider("다리 길이 (cm)", 70.0, 100.0, 80.0)
 
 X = np.array([[height, waist, hip]])
 
